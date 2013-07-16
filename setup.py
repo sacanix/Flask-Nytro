9d6070869acc85c1172e83c49d5ce156dc284344
Tony Kamillo (Sacanix) <tonysacanix@gmail.com>
2013-07-16 09:47:00 -0300

    Changing description in setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,19 @@
 Flask-Nytro
 -------------
 
-Nytro is an extension to help developer providing a set of useful tools
-giving even more facility to development apps with Flask.
+Nytro is an extension to help the developers providing a set of useful tools giving even more facility to development apps with Flask.
 """
 from setuptools import setup
 
 
 setup(
     name='Flask-Nytro',
-    version='1.1',
+    version='1.2',
     url='http://github.com/sacanix/Flask-Nytro',
     license='BSD',
     author='Tony Kamillo (Sacanix)',
     author_email='tonysacanix@gmail.com',
-    description='''Nytro is an extension to help developer providing a set of useful tools
-    giving even more facility to development apps with Flask.''',
+    description='''Nytro is an extension to help the developers providing a set of useful tools giving even more facility to development apps with Flask.''',
     long_description=__doc__,
     # if you would be using a package instead use packages instead
     # of py_modules:
